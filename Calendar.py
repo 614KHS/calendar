c7495ef40fb88c58157c1a81a4f777622281a8b7
Shutaro.K <gijhide@gmail.com>
2019-05-27 09:38:33 +0900

    [fix]先週までの状態に修正

--- Calendar.py
+++ Calendar.py
@@ -1,24 +1,25 @@
 input_count=input('好きな数を入れてね:')
 count = int(input_count)
 
-if count >= 1900 and count <= 1911:
-    print("明治" + str(33+count-1900) + "年です")
-elif count == 1912:
-    print("大正元年です")
-elif count >= 1913 and count <= 1925:
-    print("大正" + str(1+count-1912) + "年です")
-elif count == 1926:
-    print("昭和元年です")
-elif count >= 1927 and count <= 1988:
-    print("昭和" + str(1+count-1926) + "年です")
-elif count == 1989:
-    print("平成元年です")
-elif count >= 1990 and count <= 2018:
-    print("平成" + str(1+count-1989) + "年です")
-elif count == 2019:
-    print("令和元年です")
-elif count >= 2020 and count <= 2050:
-    print("令和" + str(1+count-2019) + "年です")
-elif count < 1900 and count > 2050:
+while count < 1900 and count > 2050:
     input_count = input('好きな数を入れてね:')
     count = int(input_count)
+else:
+    if count >= 1900 and count <= 1911:
+        print("明治" + str(33+count-1900) + "年です")
+    elif count == 1912:
+        print("大正元年です")
+    elif count >= 1913 and count <= 1925:
+        print("大正" + str(1+count-1912) + "年です")
+    elif count == 1926:
+        print("昭和元年です")
+    elif count >= 1927 and count <= 1988:
+        print("昭和" + str(1+count-1926) + "年です")
+    elif count == 1989:
+        print("平成元年です")
+    elif count >= 1990 and count <= 2018:
+        print("平成" + str(1+count-1989) + "年です")
+    elif count == 2019:
+        print("令和元年です")
+    elif count >= 2020 and count <= 2050:
+        print("令和" + str(1+count-2019) + "年です")
